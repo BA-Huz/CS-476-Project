b2306a0a2926335eea2a2d726c85eb5218532497
BA-Huz <56166683+BA-Huz@users.noreply.github.com>
2020-03-06 15:32:22 -0700

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    Rigidbody rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        Vector3 startPosition = new Vector3();
+        startPosition.Set(player.transform.position.x, player.transform.position.y + 1.5f, -10f);
+        transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xTranslate = 0f;
+        float yTranslate = 0f;
+
+
+        // was falling but just hit the gound
+        if(rb.isKinematic == false && player.GetComponent<CharacterMovement>().isNormalForceInDirection(1) == true)
+        {
+            rb.isKinematic = true;
+            //transform.position.Set(transform.position.x, player.transform.position.y, transform.position.z);
+            yTranslate += player.transform.position.y - transform.position.y + 1.5f;
+            //justLandedFromFar = true;
+            // resetting the camera after a far landing needs to be handled the next fram
+            // otherwise the position of the player is based off of the physics engine of 
+            // where it would be if there was no collider to land on
+        }
+
+        // move camera right
+        if ((Input.GetKey("d") || Input.GetKey("right")) && player.GetComponent<CharacterMovement>().isNormalForceInDirection(4) == false) // move camera right
+        {
+            if (player.transform.position.x + 3.0f >= transform.position.x)
+                xTranslate += 0.2f;
+            else if(player.transform.position.x - 3.0f >= transform.position.x)
+                xTranslate += 0.1f;
+        }
+        // move camera left
+        else if ((Input.GetKey("a") || Input.GetKey("left")) && player.GetComponent<CharacterMovement>().isNormalForceInDirection(2) == false) // move camera left
+        {
+            if (player.transform.position.x - 3.0f <= transform.position.x)
+                xTranslate += -0.2f;
+            else if(player.transform.position.x + 3.0f <= transform.position.x)
+                xTranslate += -0.1f;
+        }
+
+
+
+        // falling
+        if (player.transform.position.y <= transform.position.y - 3)
+        {
+            //yTranslate = player.transform.position.y + 3.0f;
+            rb.isKinematic = false;
+            //rb.velocity = player.getVelocity();
+            rb.velocity = player.GetComponent<CharacterMovement>().getVelocity() + new Vector3(0, -1, 0);
+        }
+
+        //moving up or falling far
+        if ((player.transform.position.y >= transform.position.y + 3))
+        {
+            if (rb.isKinematic == true)
+                yTranslate += 0.1f;
+            else
+                rb.velocity = player.GetComponent<CharacterMovement>().getVelocity();
+        }
+
+
+        transform.Translate(xTranslate, yTranslate, 0f);
+    }
+}
---
+++ CharacterMovement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+   // bool lastDirection; // true means left, false means right
+    Rigidbody2D rbBox;
+
+
+
+    void Awake()
+    {
+        rbBox = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbBox.AddForce(new Vector2(0, 0), ForceMode2D.Force);
+        if ((Input.GetKey("d") || Input.GetKey("right")) && ! isNormalForceInDirection(4) )
+        {
+            transform.Translate(0.1f, 0f, 0f);
+        }
+        else if ((Input.GetKey("a") || Input.GetKey("left")) && ! isNormalForceInDirection(2))
+        {
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+
+        if ((Input.GetKeyDown("w") || Input.GetKeyDown("up")) && isNormalForceInDirection(1))// && rb.GetContacts.y == -1)//isOnGround == true)
+        {
+            rbBox.AddForce(new Vector2(0, 8.5f), ForceMode2D.Impulse);
+        }
+    }
+
+    public bool isNormalForceInDirection(int direction) // 1 is up, 2 is right, 3 is down, 4 is left
+    {
+        ContactPoint2D[] contactPoints = new ContactPoint2D[20]; // this 20 is an intentional over estimate 
+        int numOfContacts = rbBox.GetContacts(contactPoints);
+
+        if (direction == 1)
+        {
+            for(int i = 0; i < numOfContacts; i++)
+            {
+                if (contactPoints[i].normal.y >= 1) // if on any contact points the y axis is experienceing an upwards normal force, ie it is on top of an object
+                {
+                    return true;
+                }
+            }
+        }
+        else if (direction == 2)
+        {
+            for(int i = 0; i < numOfContacts; i++)
+            {
+                if (contactPoints[i].normal.x >= 1)
+                {
+                    return true;
+                }
+            }
+        }
+        else if (direction == 3)
+        {
+            for(int i = 0; i < numOfContacts; i++)
+            {
+                if (contactPoints[i].normal.y <= -1)
+                {
+                    return true;
+                }
+            }
+        }
+        else if (direction == 4)
+        {
+            for(int i = 0; i < numOfContacts; i++)
+            {
+                if (contactPoints[i].normal.x <= -1)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void OnCollisionStay2D(Collision2D collision) // detects collisons on side as being on ground
+    {
+        //isOnGround = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        //isOnGround = false;
+    }
+
+    public Vector3 getVelocity()
+    {
+        return rbBox.velocity;
+    }
+}
