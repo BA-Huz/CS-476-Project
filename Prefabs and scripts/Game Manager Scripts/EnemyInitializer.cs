010e7126f2f05c5c3424d7b615b758c7493d9e7d
BA-Huz <56166683+BA-Huz@users.noreply.github.com>
2020-03-24 13:49:59 -0600

    Add files via upload

--- Prefabs and scripts/Game Manager Scripts/EnemyInitializer.cs
+++ Prefabs and scripts/Game Manager Scripts/EnemyInitializer.cs
@@ -6,17 +6,21 @@ public class EnemyInitializer : MonoBehaviour
 {
     public GameObject greenSlime; // the game object of green slime
     public Vector3[] greenSlimeLocations; // the array of vector 3s corosponding with the positions to spawn in green slimes
-    GameObject[] greenSlimes; // for use  when deleting old dashs
+    GameObject[] greenSlimes; // for use  when deleting old green slimes
+    public GameObject purpleSlime; // the game object of purple slime
+    public Vector3[] purpleSlimeLocations; // the array of vector 3s corosponding with the positions to spawn in purple slimes
+    GameObject[] purpleSlimes; // for use  when deleting old purple slimes
     
     void Awake()
     {
         greenSlimes = new GameObject[0];
+        purpleSlimes = new GameObject[0];
         InitializeEnemies();
     }
 
     public void InitializeEnemies()
     {
-        // this if and nested loop ensures their arent any currently existing slimes
+        // this if and nested loop ensures their arent any currently existing green slimes
         if (greenSlimes.Length > 0)
         {
             foreach (GameObject slime in greenSlimes)
@@ -34,6 +38,25 @@ public class EnemyInitializer : MonoBehaviour
             location.Set(location.x, location.y, -0.1f);
             greenSlimes[i] = Instantiate(greenSlime, location, roation);
             greenSlimes[i++].GetComponent<GreenSlimeBehaviour>().player = transform.GetChild(0).gameObject;
+        }
+
+        // this if and nested loop ensures their arent any currently existing purple slimes
+        if (purpleSlimes.Length > 0)
+        {
+            foreach (GameObject slime in purpleSlimes)
+            {
+                Destroy(slime);
+            }
+        }
+
+        // spawn in each purple slime
+        purpleSlimes = new GameObject[purpleSlimeLocations.Length];
+        i = 0;
+        foreach(Vector3 location in purpleSlimeLocations)
+        {
+            location.Set(location.x, location.y, -0.1f);
+            purpleSlimes[i] = Instantiate(purpleSlime, location, roation);
+            purpleSlimes[i++].GetComponent<PurpleSlimeBehaviour>().player = transform.GetChild(0).gameObject;
         }
     }
 }
